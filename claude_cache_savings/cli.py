dfaeab97c391578beed43d90458fd690fe59b12d
Jonas Tingeborn <tinjon@gmail.com>
2025-10-17 22:44:52 +0200

    Refactor out reporting function from main (cosmetic change)

--- claude_cache_savings/cli.py
+++ claude_cache_savings/cli.py
@@ -21,21 +21,6 @@ def main():
     files = [f for p in opts.files for f in glob(p)]
     stats = calculate_savings(files)
 
-    def report(stats, indent=0):
-        def log(*args):
-            print((' '*indent), *args, sep='')
-        log(f'Input cache hit   {stats.hitrate * 100:.0f} %')
-        log(f'Input savings     {stats.input_savings * 100:.0f} %')
-        log(f'Total savings     {stats.total_savings * 100:.0f} %')
-        log()
-        log(f'Total cost        $ {stats.total_cost:.2f}')
-        log(f'Est cache-less    $ {stats.total_cost_est:.2f}')
-        log()
-        itt, ott = f'{stats.input_tokens_total:,}', f'{stats.output_tokens_total:,}'
-        log(f'Input tokens      {itt:<12s} | $ {stats.input_cost:.2f}')
-        log(f'Output tokens     {ott:<12s} | $ {stats.output_cost:.2f}')
-        log(f'Input/output tok  {stats.input_tokens_total / stats.output_tokens_total:.0f}x')
-
     print('==[ Total ]' + '='*36 + '\n')
     report(stats)
 
@@ -50,6 +35,24 @@ def main():
             stats.output_tokens_total + stats.input_tokens_total)
         print(f'\n{model_id:26}  (use-cost: {use_frac*100:.0f}% {cost_frac*100:.0f}%)')
         report(mstat, 2)
+
+
+def report(stats, indent=0):
+    def log(*args):
+        print((' '*indent), *args, sep='')
+
+    log(f'Input cache hit   {stats.hitrate * 100:.0f} %')
+    log(f'Input savings     {stats.input_savings * 100:.0f} %')
+    log(f'Total savings     {stats.total_savings * 100:.0f} %')
+    log()
+    log(f'Total cost        $ {stats.total_cost:.2f}')
+    log(f'Est cache-less    $ {stats.total_cost_est:.2f}')
+    log()
+    itt, ott = f'{stats.input_tokens_total:,}', f'{stats.output_tokens_total:,}'
+    log(f'Input tokens      {itt:<12s} | $ {stats.input_cost:.2f}')
+    log(f'Output tokens     {ott:<12s} | $ {stats.output_cost:.2f}')
+    log(f'Input/output tok  {stats.input_tokens_total / stats.output_tokens_total:.0f}x')
+
 
 if __name__ == '__main__':
     main()
